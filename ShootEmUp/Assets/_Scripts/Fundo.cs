f0b8964367d20b0d1b518beec27b639b45680a99
Gabriela Caruso <gabriela.caruso@yahoo.com.br>
2021-03-16 02:27:18 -0300

    projeto finalizado

---
+++ ShootEmUp/Assets/_Scripts/Fundo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fundo : MonoBehaviour
+{
+    private float offset;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        offset += (Time.deltaTime / 20.0f);
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
